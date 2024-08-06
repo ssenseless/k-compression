30e93bff67cf4160e3f03a410542ddc856c13958
ssenseless <major.add.9@ou.edu>
2024-08-06 16:40:42 -0500

    full documentation and finalizing

---
+++ k-compressor.py
@@ -0,0 +1,318 @@
+from tkinter import *
+from tkinter import filedialog  # don't know why this has to be separate, but it does
+import numpy as np
+from PIL import Image, ImageTk
+from kmeans import process_image
+
+
+# defs ---------------------------------------------------------------------------------------
+def choose_file() -> None:
+    """
+    helper function to set the global filename for the gui
+    """
+    global filename
+    filename = filedialog.askopenfilename(
+        initialdir='/',
+        title='Select an image',
+        filetypes=[
+            ('Image files', '*.png *.jpg')
+        ]
+    )
+
+    if filename != '':
+        flavor_text.set('Now change the amount of colors to compress to, then confirm')
+
+    else:
+        flavor_text.set('Must select either .png or .jpg')
+
+
+def resize_image() -> Image:
+    """
+    helper function to resize images and retain aspect ratio
+    :return: resized image
+    """
+    unaltered = Image.open(fp=filename)
+    aspect = 300 / max(unaltered.height, unaltered.width)
+
+    return unaltered.resize(
+        size=(
+            int(unaltered.width * aspect),
+            int(unaltered.height * aspect)
+        )
+    )
+
+
+def reset_colors() -> None:
+    """
+    helper function to reset color boxes and texts so
+    they properly update when changing k or image
+    """
+    for i in range(6):
+        reset_image = ImageTk.PhotoImage(
+            Image.new(
+                mode='RGB',
+                size=(50, 50),
+                color=(200, 200, 200)
+            )
+        )
+        color_boxes[i].config(image=reset_image)
+        color_boxes[i].image = reset_image
+        color_texts[i].set("")
+
+        percent_texts[i].set("")
+
+
+def set_colors(centroids: np.ndarray, percents: np.ndarray, k: int) -> None:
+    """
+    helper function to update the color boxes, texts, and
+    percent texts after they have been reset
+    :param centroids: six most prevalent centroids (colors)
+    :param percents: percentage of pixels associated with each centroid
+    :param k: total number of centroids (colors)
+    """
+    # make an image, hex tag, and percentage for each centroid
+    for i, centroid in enumerate(centroids):
+        centroid_color = ImageTk.PhotoImage(
+            Image.new(
+                mode='RGB',
+                size=(50, 50),
+                color=tuple(centroid)
+            )
+        )
+        color_boxes[i].config(image=centroid_color)
+        color_boxes[i].image = centroid_color
+        color_texts[i].set(f"#{centroid[0]:02x}{centroid[1]:02x}{centroid[2]:02x}")
+
+        percent_texts[i].set(f"{round(percents[i], 2)}%")
+
+    # set remaining percentage for remaining (nonspecific) "centroids"
+    if k > 6:
+        remaining_explain.set(f"Remaining {k - 6} colors")
+    else:
+        remaining_explain.set(f"Remaining 0 colors")
+    remaining_percent.set(f"{round(100 - sum(percents), 2)}%")
+
+
+def compress() -> None:
+    """
+    general function to run kmeans compression and 
+    helper functions on click
+    """
+    k = k_input.get()
+    
+    # catch user edge cases
+    if filename == '':
+        flavor_text.set('Must select a photo before confirming pixel amount')
+    elif k == 0:
+        flavor_text.set('Color number must be greater than zero')
+    else:
+        global pillow_image_postcompress  # once again not entirely sure why this one has to be here
+        
+        # get user selected image, resize, run kmeans, update application with helper functions
+        choice = resize_image()
+        compressed, centroids, percents = process_image(choice, filename[-3:] == 'jpg', k)
+
+        choice_tk = ImageTk.PhotoImage(choice)
+        precompress_frame.config(image=choice_tk)
+        precompress_frame.image = choice_tk
+
+        compressed_tk = ImageTk.PhotoImage(Image.fromarray(compressed))
+        postcompress_frame.config(image=compressed_tk)
+        postcompress_frame.image = compressed_tk
+        pillow_image_postcompress = Image.fromarray(compressed).resize((1000, 1000))
+
+        reset_colors()
+        set_colors(centroids, percents, k)
+
+
+def save() -> None:
+    """
+    allow users to save compressed photos as 1000x1000 .jpg or .png
+    """
+    # todo: allow user to resize image to specifications? aspect ratio?
+    path = filedialog.asksaveasfilename(
+        initialdir='/',
+        title='Select a save location',
+        confirmoverwrite=True,
+        filetypes=[
+            ('Image files', '.jpg')
+        ],
+        defaultextension="*.*"
+    )
+    
+    # user edge case handle
+    if path == '':
+        flavor_text.set("Must select directory location")
+    else:
+        pillow_image_postcompress.save(path)
+        flavor_text.set("Saved!")
+
+
+# globals ------------------------------------------------------------------------------------
+back_col = '#343434'
+filename = ''
+
+# simple initializers ------------------------------------------------------------------------
+root = Tk()
+root.title('k-means image compression')
+root.config(bg=back_col)
+root.geometry('1000x600')
+
+# pillowo ------------------------------------------------------------------------------------
+
+# images and image frames
+image_precompress = ImageTk.PhotoImage(
+    Image.new(
+        mode='RGB',
+        size=(300, 300),
+        color=(200, 200, 200)
+    )
+)
+pillow_image_postcompress = Image.new(
+    mode='RGB',
+    size=(300, 300),
+    color=(200, 200, 200)
+)
+image_postcompress = ImageTk.PhotoImage(pillow_image_postcompress)
+
+precompress_frame = Label(image=image_precompress)
+postcompress_frame = Label(image=image_postcompress)
+
+# color boxes and images
+image_colors = [
+    ImageTk.PhotoImage(
+        Image.new(
+            mode='RGB',
+            size=(50, 50),
+            color=(200, 200, 200)
+        )
+    )
+    for _ in range(6)
+]
+color_boxes = [Label(image=image_colors[i]) for i in range(6)]
+
+# component initializers ---------------------------------------------------------------------
+# labels for images
+save_image = Button(
+    root,
+    text="Save compressed image",
+    command=save
+)
+immutable1 = Label(
+    root,
+    bg=back_col,
+    text='Uncompressed image',
+    font=('Arial', 7)
+)
+immutable2 = Label(
+    root,
+    bg=back_col,
+    text='Compressed image',
+    font=('Arial', 7)
+)
+
+# helper text for user
+flavor_text = StringVar()
+flavor_text_label = Label(
+    root,
+    bg=back_col,
+    textvariable=flavor_text
+)
+flavor_text.set('Select an image below to get started')
+
+# button to select image
+image_button = Button(
+    root,
+    text='Browse Files',
+    command=choose_file
+)
+
+# input box and button to choose k for k-means
+k_input = IntVar()
+k_input_entry = Entry(
+    root,
+    textvariable=k_input
+)
+confirm_button = Button(
+    root,
+    text='Confirm',
+    command=compress
+)
+
+# color boxes to show user (up to) 6 colors in photo
+color_box_label = Label(
+    root,
+    bg=back_col,
+    text='Colors',
+    font=('Arial', 10)
+)
+color_texts = [StringVar() for _ in range(6)]
+color_text_labels = [
+    Label(
+        root,
+        bg=back_col,
+        textvariable=color_texts[i]
+    )
+    for i in range(6)
+]
+
+# percentage of centroids in picture labels
+percent_texts = [StringVar() for _ in range(6)]
+percent_text_labels = [
+    Label(
+        root,
+        bg=back_col,
+        textvariable=percent_texts[i]
+    )
+    for i in range(6)
+]
+remaining_percent = StringVar()
+remaining_percent_label = Label(
+    root,
+    bg=back_col,
+    textvariable=remaining_percent
+)
+remaining_explain = StringVar()
+remaining_explain_label = Label(
+    root,
+    bg=back_col,
+    textvariable=remaining_explain
+)
+
+# grid ---------------------------------------------------------------------------------------
+# images
+precompress_frame.grid(column=1, row=0, columnspan=3, padx=5, pady=(5, 0))
+postcompress_frame.grid(column=4, row=0, columnspan=3, padx=5, pady=(5, 0))
+save_image.grid(column=7, row=0)
+
+# image labels
+immutable1.grid(column=1, row=1, columnspan=3, padx=5)
+immutable2.grid(column=4, row=1, columnspan=3, padx=5)
+
+# user helper text
+flavor_text_label.grid(column=1, row=2, columnspan=6, pady=3)
+
+# image selection button
+image_button.grid(column=1, row=3, columnspan=6, pady=5)
+
+# user input k
+k_input_entry.grid(column=1, row=4, columnspan=3, sticky='e', pady=5, padx=(0, 5))
+confirm_button.grid(column=4, row=4, columnspan=3, sticky='w', pady=5, padx=(5, 0))
+
+# color boxes
+color_box_label.grid(column=1, row=5, columnspan=6, pady=(1, 4))
+for i in range(6):
+    color_boxes[i].grid(column=i + 1, row=6)
+    color_text_labels[i].grid(column=i + 1, row=7)
+    percent_text_labels[i].grid(column=i + 1, row=8)
+
+remaining_explain_label.grid(column=7, row=7)
+remaining_percent_label.grid(column=7, row=8)
+
+
+# spacing on outsides
+root.grid_columnconfigure((0, 7), weight=1)
+
+
+if __name__ == "__main__":
+    root.mainloop()
--- kmeans.py
+++ kmeans.py
@@ -1,95 +1,175 @@
 import time
 import numpy as np
+from PIL import Image
 
 
-# defs
 def timer(f, comment="not given"):
+    """
+    simple function that times other functions
+    mostly utilized to test different vectorizations for optimality
+    :param f: function to be timed
+    :param comment: some helpful identifying information about the function in console
+    :return: identical to the return of the timed function
+    """
     a = time.time()
     retval = f()
     b = time.time()
 
-    print(f"Time difference (Func: {comment}): {round(b - a, 6)} sec.")
+    print(f"Time difference (Func: {comment}): {round(b - a, 4)} sec.")
     return retval
 
 
-def find_closest_centroids(data, centroids):
-    return np.argmin(
-        np.linalg.norm(
-            np.tile(
+def find_closest_centroids(data: np.ndarray, centroids: np.ndarray) -> np.ndarray:
+    """
+    uses L2 norm to calculate the closest centroid to each data point
+    :param data: input data (image) of shape [height * width, 3]
+    :param centroids: current k centroids to map pixels to
+    :return: the centroid indices of each pixel (shape identical to data)
+    """
+    return np.argmin(  # minimal index
+        np.linalg.norm(  # of the l2 norm between
+            np.tile(  # each data point
                 A=data[..., None],
                 reps=centroids.shape[0]
-            ) - centroids.T,
+            ) - centroids.T,  # and each centroid
             axis=1
         ),
         axis=1
     )
 
 
-def compute_centroids(data, idx, k):
+def compute_centroids(data: np.ndarray, idx: np.ndarray, k: int) -> np.ndarray:
+    """
+    creates new centroids by computing the mean of
+    the pixels assigned to each current centroid
+    :param data: input data (image) of shape [height * width, 3]
+    :param idx: array of indices mapping [index (pixel) -> value at index (centroid)]
+    :param k: number of centroids
+    :return: new centroids
+    """
+    # m is height * width   (pixels)
+    # n SHOULD be 3         (R G B)
     m, n = data.shape
     centroids = np.zeros((k, n))
+
+    # make a dictionary pointing from each unique centroid (0 to k - 1)
+    # to the amount of pixels associated with that centroid
+    # if count is zero then the centroid will not exist
     unique, counts = np.unique(idx, return_counts=True)
     counts = dict(zip(unique, counts))
 
+    # first add each data value to its associated centroid
     for i in range(m):
         centroids[idx[i]] += data[i]
 
+    # then subtract it by the amount of pixels associated with the centroid
     missing_centroid = 0
     for index in unique:
+        # catch up from missing indices and correctly report centroid value as zero
         while missing_centroid != index:
             centroids[missing_centroid] = 0
             missing_centroid += 1
+
         centroids[index] /= counts[index]
         missing_centroid += 1
 
     return centroids
 
 
-def top_centroid_percent(idx, centroids):
+def top_centroid_percent(idx: np.ndarray, centroids: np.ndarray) -> (np.ndarray, list):
+    """
+    return the most prevalent six centroids (colors) and the corresponding percentage
+    of the total picture they color in
+    :param idx: array of indices mapping [index (pixel) -> value at index (centroid)]
+    :param centroids: current k centroids (colors)
+    :return: the most used six centroids and the percentage of the picture they color in
+    """
+    # total number of pixels in image
     total = idx.shape[0]
 
+    # make a dictionary pointing from each unique centroid (0 to k - 1)
+    # to the amount of pixels associated with that centroid
     unique, counts = np.unique(idx, return_counts=True)
     counts = dict(zip(unique, counts))
 
+    # get most prevalent values from dictionary
+    # (sort dictionary by value, backwards, and return top six keys)
     top_6 = sorted(counts, key=counts.get, reverse=True)[:6]
+
+    # get centroids at each key and calculate respective percentages
     centroids = np.array([centroids[index] for index in top_6])
     percents = [(counts[index] / total) * 100 for index in top_6]
 
     return centroids, percents
 
 
-def find_k_means(data, initial_centroids, max_iters=10):
+def find_k_means(data: np.ndarray, initial_centroids: np.ndarray, max_iters=10) -> (np.ndarray, np.ndarray):
+    """
+    finds the closest centroids and the pixels
+    associated with them in under (max_iters) iterations
+    :param data: input data (image) of shape [height * width, 3]
+    :param initial_centroids: some initialization of centroids (see initialize_rand_centroids())
+    :param max_iters: maximal number of iterations that the algorithm will take before stopping
+                      (max_iters is the only stopping condition for this function as there is no
+                      epsilon/delta termination functionality)
+    :return: k centroids (colors), and the indices of the pixels associated with them
+    """
     centroids = initial_centroids
 
+    # iterate back and forth, replacing the indices, then the centroids, until (max_iters)
     for i in range(max_iters):
         idx = find_closest_centroids(data, centroids)
         centroids = compute_centroids(data=data, idx=idx, k=initial_centroids.shape[0])
 
-    centroids, percents = top_centroid_percent(idx, centroids)
-    return centroids, percents, idx
+    return centroids, idx
 
 
-def initialize_rand_centroids(data, k):
+def initialize_rand_centroids(data: np.ndarray, k: int) -> np.ndarray:
+    """
+    returns k randomly selected centroids from (data)
+    :param data: input data (image) of shape [height * width, 3]
+    :param k: number of centroids
+    :return: random permutation of (k) pixels from (data)
+    """
     return data[np.random.permutation(data.shape[0])[:k]]
 
 
-def process_image(image, is_jpg, k):
+def process_image(image: Image, is_jpg: bool, k: int) -> (np.ndarray, np.ndarray, list):
+    """
+    given an image, return six centroids, the percentage of
+    the image that they make up, and the image (as an array)
+    with the centroid colors replacing their respective pixels
+    (this function is the entry point for the file)
+    :param image: a pillow Image containing all pixel data, height, width, etc...
+    :param is_jpg: whether the image is a .jpg or .png file (.png colormaps are already 0 - 1)
+    :param k: number of centroids (colors) to compress image into
+    :return:
+             the processed image (as an array with shape [height * width, 3]),
+             the six most prevalent centroids (colors),
+             the centroids respective percentage of the image
+    """
+    # need numpy goodness instead of whatever pillow offers
     processing = np.array(image)
 
+    # .png colormaps are 0 - 1 scaled already
     if is_jpg:
         processing = processing / 255
 
+    # turn 2d image into height * width list of pixels
     processing_reshape = np.reshape(processing, (processing.shape[0] * processing.shape[1], 3))
-    centroids, percents, idx = find_k_means(
+
+    # get the centroids, map the original pixels to a centroid, reshape back into a 2d image
+    centroids, idx = find_k_means(
         data=processing_reshape,
         initial_centroids=initialize_rand_centroids(
             data=processing_reshape,
             k=k
         ),
-        max_iters=5
+        max_iters=10
     )
-
-    idx = find_closest_centroids(processing_reshape, centroids)
     processed = np.uint8(np.reshape(centroids[idx, :], processing.shape) * 255)
+
+    # quickly grab the top six centroids and their percentage of the image
+    centroids, percents = top_centroid_percent(idx, centroids)
 
     return processed, np.uint8(centroids * 255), percents
